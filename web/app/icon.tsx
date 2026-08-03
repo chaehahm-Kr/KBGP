81f084842c9bde873c75b690924c537ebcf90944
chaehahm-Kr <chaehahm@gmail.com>
2026-08-03 11:02:38 -0400

    Refine K-symbol to completely isolate checkmark from frame with spacing and keep it inside bounding box

--- web/app/icon.tsx
+++ web/app/icon.tsx
@@ -28,24 +28,24 @@ export default function Icon() {
         >
           {/* 사각형 프레임 */}
           <path
-            d="M 112 162 H 38 V 38 H 162 V 112"
+            d="M 112 170 H 30 V 30 H 170 V 112"
             stroke="#111111"
             strokeWidth="16"
-            strokeLinecap="square"
+            strokeLinecap="butt"
             strokeLinejoin="miter"
             fill="none"
           />
           {/* K 자 */}
           <path
-            d="M 72 60 H 90 V 98 L 123 60 H 142 L 105 100 L 143 140 H 124 L 90 102 V 140 H 72 Z"
+            d="M 70 55 H 88 V 95 L 123 55 H 142 L 103 98 L 143 145 H 124 L 88 103 V 145 H 70 Z"
             fill="#111111"
           />
           {/* 체크 마크 */}
           <path
-            d="M 105 142 L 132 168 L 174 114"
+            d="M 120 148 L 142 170 L 170 120"
             stroke="#8a93a6"
             strokeWidth="16"
-            strokeLinecap="square"
+            strokeLinecap="butt"
             strokeLinejoin="miter"
             fill="none"
           />
